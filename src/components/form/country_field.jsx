43b7245b8e8b2c0ee9338e172ee99905db161c78
KevinSlim67 <kevinslim167@gmail.com>
2022-07-06 21:28:36 +0300

    fixed countries and regions not working on most browsers

--- src/components/form/country_field.jsx
+++ src/components/form/country_field.jsx
@@ -3,20 +3,26 @@ import { useDispatch } from "react-redux";
 import { changeCountry } from "../../features/selectedLocation.js";
 import { Country } from "country-state-city";
 
+function getCountryByCode(country) {
+  const countryCodes = Country.getAllCountries().filter(
+    (c) => c.name === country
+  );
+  return countryCodes[0].isoCode;
+}
+
 function CountryField(props) {
-  
   //create an array of all countrie's name, code and flag
   const countries = [
     ...Country.getAllCountries().map((c) => {
-      return { name: c.name, isoCode: c.isoCode, flag: c.flag};
+      return { name: c.name, isoCode: c.isoCode, flag: c.flag };
     }),
   ];
 
-  
   const dispatch = useDispatch();
   //changes the global state of country to the value in the parameter
-  const handleSelection = (value) => {
-    dispatch(changeCountry({ country: value }));
+  const handleSelection = (e) => {
+    const code = getCountryByCode(e.target.value);
+    dispatch(changeCountry({ country: code }));
   };
 
   return (
@@ -30,14 +36,14 @@ function CountryField(props) {
       </label>
 
       <div className="form-select w-full">
-        <select id="country" name="country" required={true}>
+        <select
+          id="country"
+          name="country"
+          required={true}
+          onChange={handleSelection}
+        >
           {countries.map((c) => (
-            <option
-              key={c.isoCode}
-              value={c.name}
-              className="w-full"
-              onClick={() => handleSelection(c.isoCode)}
-            >
+            <option key={c.isoCode} value={c.name} className="w-full">
               {c.flag} {c.name}
             </option>
           ))}
--- src/components/form/state_region.jsx
+++ src/components/form/state_region.jsx
@@ -3,14 +3,20 @@ import { useDispatch, useSelector } from "react-redux";
 import { changeRegion } from "../../features/selectedLocation.js";
 import { State } from "country-state-city";
 
+function getRegionByCode(state) {
+  const regionCodes = State.getAllStates().filter((s) => s.name === state);
+  return regionCodes[0].isoCode;
+}
+
 function StateRegionField(props) {
   //gets the current selected country; if no country is selected, country will === null
   const countryCode = useSelector((state) => state.selectedLocation.country);
 
   const dispatch = useDispatch();
   //changes the global state of region to the value in the parameter
-  const handleSelection = (value) => {
-    dispatch(changeRegion({ region: value }));
+  const handleSelection = (e) => {
+    const code = getRegionByCode(e.target.value);
+    dispatch(changeRegion({ region: code }));
   };
 
   const regions = State.getStatesOfCountry(countryCode);
@@ -31,12 +37,12 @@ function StateRegionField(props) {
           name="state-province"
           id="state-province"
           required={true}
+          onChange={handleSelection}
         >
           {regions.map((r) => (
             <option
               key={`${countryCode}-${r.isoCode}`}
               value={r.name}
-              onClick={() => handleSelection(r.isoCode)}
             >
               {r.name}
             </option>
