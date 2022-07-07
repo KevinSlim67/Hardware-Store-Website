038ee22af25236241c32c47b756ca18a97497058
KevinSlim67 <kevinslim167@gmail.com>
2022-07-07 17:20:38 +0300

    fixed url

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
-//const url = 'https://hardware-store-server.herokuapp.com';
-const url = 'http://localhost:5000';
+const url = 'https://hardware-store-server.herokuapp.com';
+//const url = 'http://localhost:5000';
 
 module.exports = { url };
