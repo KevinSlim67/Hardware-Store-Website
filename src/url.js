09bf5c0400ca9d23c822598e38e1ba73f87e4796
KevinSlim67 <kevinslim167@gmail.com>
2022-12-12 20:49:45 +0200

    updated url

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
-const url = 'https://hardware-store-server.herokuapp.com';
+const url = 'https://hardware-store-server-production.up.railway.app';
 //const url = 'http://localhost:5000';
 
 module.exports = { url };
